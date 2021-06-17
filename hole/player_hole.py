605655e6652dc0f80248859bc8b15ee78592849d
Evgeny Grebnev <egrebnev@infinidat.com>
2021-06-17 18:25:13 +0300

    preset player hole rock number is 0

--- hole/player_hole.py
+++ hole/player_hole.py
@@ -6,7 +6,7 @@ from hole import Hole
 class PlayerHole(Hole):
     def __init__(self, player: str, index: Union[str, int], number_of_rocks, linked_hole=None):
         super().__init__(player, index, number_of_rocks, linked_hole)
-        self.number_of_rocks = 11
+        self.number_of_rocks = 0
 
     def take_rocks(self, name=None):
         hole = self.get_by_name(name)
